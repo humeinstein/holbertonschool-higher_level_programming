44d15c92259cfd75520e53651846260d7502e90c
humeinstein <humeincreations@gmail.com>
2019-09-10 23:58:39 +0000

    [INIT]function that prints a string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for upc in str:
+        if ord(upc) >= ord('a') and ord(upc) <= ord('z'):
+            upc = chr(ord(upc) - 32)
+        print("{:s}".format(upc), end="")
+    print("")
