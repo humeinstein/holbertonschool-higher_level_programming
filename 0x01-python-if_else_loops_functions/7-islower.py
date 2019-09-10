08cf757f242e2dcf334559dc8171f6c53fcc37bf
humeinstein <humeincreations@gmail.com>
2019-09-10 20:49:00 +0000

    [TEST]

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if(c >= 'A' and c <= 'Z'):
-        print("{:c} is upper".format(c))
-        return False
-    else:
+    if(c >= 'a' and c <= 'z'):
         return True
+    else:
+        return False
