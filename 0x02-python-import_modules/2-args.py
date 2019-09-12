3fcf4dbbee6d1ef887b52bbbc2676fe37cf6bf04
humeinstein <humeincreations@gmail.com>
2019-09-12 18:51:27 +0000

    [BUGFIX]forgot if __name__ == __main__:

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-arguments = len(sys.argv)
-if arguments == 1:
-    print("{:d} arguments.".format(arguments - 1))
-elif arguments == 2:
-    print("{:d} argument:".format(arguments - 1))
-else:
-    print("{:d} arguments:".format(arguments - 1))
+    arguments = len(sys.argv)
+    if arguments == 1:
+        print("{:d} arguments.".format(arguments - 1))
+    elif arguments == 2:
+        print("{:d} argument:".format(arguments - 1))
+    else:
+        print("{:d} arguments:".format(arguments - 1))
 
-x = 1
-while x != arguments:
-    print("{:d}: %s".format(x) % str(sys.argv[x]))
-    x = x + 1
+    x = 1
+    while x != arguments:
+        print("{:d}: %s".format(x) % str(sys.argv[x]))
+        x = x + 1
