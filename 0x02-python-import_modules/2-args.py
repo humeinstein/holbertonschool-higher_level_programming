79d9db667899be9ffd6c533fd4b5beba94bf437e
humeinstein <humeincreations@gmail.com>
2019-09-12 18:38:01 +0000

    [INIT]program that prints number of argv and content

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-
-print("This is the name of the script %s" % (sys.argv[0]))
-
-print("The arguments are : %s" % str(sys.argv))
-
-
-
 arguments = len(sys.argv)
 if arguments == 0:
     print("{:d} arguments:".format(arguments - 1))
