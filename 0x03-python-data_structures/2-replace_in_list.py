3187081a4ad65726f5c431c2bdf589037d331d56
humeinstein <humeincreations@gmail.com>
2019-09-17 13:03:57 -0400

    [tet]

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -8,4 +8,5 @@ def replace_in_list(my_list, idx, element):
         for x, i in enumerate(my_list):
             if i == idx:
                 my_list[i] = element
-        return(my_list) 
+                new_list = my_list
+        return(new_list) 
