824c033382181290c070232675fb087ea4475bec
humeinstein <humeincreations@gmail.com>
2019-09-17 12:55:35 -0400

    [BUGFIX]len - 1

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,10 +2,9 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return(my_list)
-    if idx > len(my_list):
+    if idx > len(my_list) - 1:
         return(my_list)
     for x, i in enumerate(my_list):
         if i == idx:
             my_list[x + 1] = element
-
     return(my_list)
