782ca8c1d6190bd8717893787c499291cd34affe
humeinstein <humeincreations@gmail.com>
2019-09-17 12:58:06 -0400

    [BF]restructure -> if elif else

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,10 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return(my_list)
-    if idx > len(my_list) - 1:
+    elif idx > len(my_list) - 1:
         return(my_list)
-    for x, i in enumerate(my_list):
-        if i == idx:
-            my_list[x + 1] = element
-    return(my_list)
+    else:
+        for x, i in enumerate(my_list):
+            if i == idx:
+                my_list[x + 1] = element
+        return(my_list) 
