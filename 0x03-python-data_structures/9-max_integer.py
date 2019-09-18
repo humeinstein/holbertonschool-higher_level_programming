9f458a663f25f482aa1a8aca2d6d23135b69cf1c
humeinstein <humeincreations@gmail.com>
2019-09-17 22:21:23 -0400

    GOT IT

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None:
+        return
+    x = 0
+    for o in my_list[::1]:
+        if o > x:
+            x = o           
+    return(x)
+        
