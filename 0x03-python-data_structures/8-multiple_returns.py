a75555742122c00181a8b5b2d1a7df1a7b001267
humeinstein <humeincreations@gmail.com>
2019-09-17 21:21:14 -0400

    [INIT]function that returns a tuple with the length of a string and its first character

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    total = len(sentence)
+    if total > 0:
+        hee = sentence[0]
+    else:
+        return None
+    heehee = total, hee
+    return(heehee)
