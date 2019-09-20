bb07d926fbb19c2d769934c10ee257dd5b359a1d
humeinstein <humeincreations@gmail.com>
2019-09-19 21:03:41 -0400

    [WORKING]

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    maxkey = max(a_dictionary, key=a_dictionary.get)
-    
-    if not a_dictionary.get:
-        return None
-    else:
-        return maxkey
-   
+    return max(a_dictionary if a_dictionary else None)
