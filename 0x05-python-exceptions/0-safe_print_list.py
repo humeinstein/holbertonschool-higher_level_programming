7bd5bb6a3b35d3a8fd8221b284bb73e6c6eb5878
humeinstein <humeincreations@gmail.com>
2019-09-30 13:24:30 -0400

    [INIT]functopn that prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    index = 0
+    try:
+        while index < x:
+            print("{}".format(my_list[index]), end="")
+            index += 1
+        print()
+    except IndexError:
+        pass
+    return(index)
