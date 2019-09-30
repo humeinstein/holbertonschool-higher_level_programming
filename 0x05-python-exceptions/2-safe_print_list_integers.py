95ad2efeea36420cbf405c8d9f5ca0681316b4a3
humeinstein <humeincreations@gmail.com>
2019-09-30 15:04:05 -0400

    Goin stron

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         while index < x:
             if isinstance(my_list[index], int) == True:
-                print("{}".format(my_list[index]), end="")
+                print("{:d}".format(my_list[index]), end="")
                 realindex += 1
             index += 1
         print()
