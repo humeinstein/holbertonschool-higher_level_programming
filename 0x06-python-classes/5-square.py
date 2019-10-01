b1e8cb26e910020f1b521cb7b580041fe9944037
humeinstein <humeincreations@gmail.com>
2019-10-01 15:42:54 -0400

    [INIT]same but print area

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size           
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+    
+        ###check if size is int###
+        if isinstance(value, int) == False:
+            raise TypeError("size must be an integer")
+        ###check if size is greater or eq to 0###
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        ###passed checks so execute###
+        else:
+            self.__size = value 
+    def area(self):
+        ###area needs privae __size
+        return self.__size * self.__size
+    def my_print(self):
+        for x in range(0, self.__size):
+            for x in range(0, self.__size):
+                print("#".format(), end="")
+            print("#")
+        if self.__size == 0:
+            print()
