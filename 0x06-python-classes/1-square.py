1910ccc9b68c62b46fbb1993b821f818d8d73e46
humeinstein <humeincreations@gmail.com>
2019-10-01 13:24:08 -0400

    [INIT]class Square defines square with priv inst size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    #########__init__ is Square/ size needed#####
+    def __init__(self, size):
+        #####private instance attribute####
+        self.__size = size
