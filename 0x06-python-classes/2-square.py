8670f7d321797676dc1b3dc4d3148a2cf92b91f9
humeinstein <humeincreations@gmail.com>
2019-10-01 14:29:49 -0400

    [INIT]class that defines a square||checks for type & value errors

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    ##init self size
+    def __init__(self, size=0):
+        ###check if size is int
+        if isinstance(size, int) == False:
+            raise TypeError("size must be an integer")
+        ###check if size is greater or eq to 0
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        ###passed checks so execute
+        else:
+            self.__size = size
+            
