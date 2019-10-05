1a7d953ba215e842d1123f602131ba43259d897e
humeinstein <humeincreations@gmail.com>
2019-10-05 00:04:37 -0400

    [INIT]status: not working

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    Tests for Max Integer
+    """
+    
+
+
+"""
+allows run all test code by running file 
+"""
+    __name__ == '__main__':
+    unittest.main()
