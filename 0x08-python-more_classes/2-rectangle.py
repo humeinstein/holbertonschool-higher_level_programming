400488c29b3b8e260cdd1adc84a0b627000c220d
humeinstein <humeincreations@gmail.com>
2019-10-07 11:23:38 -0400

    [INIT]built off of 1-rectangle: add methods area & perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+#!/usr/bin/python3
+"""
+init Rectangle()
+
+allow init with width and height predetermined to 0 on default
+width and height values 
+
+"""
+class Rectangle():
+    """
+    class rectangle defined by width and height. 
+    """
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.width
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self, value):
+
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise TypeError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+    def perimeter(self):
+        newvalue1 = self.__height + self.__height
+        newvalue2 = self.__width + self.__width
+        return newvalue1 + newvalue2 
