d4b8084b19610edbb38c39db1aa6947f8b40924d
humeinstein <humeincreations@gmail.com>
2019-10-16 19:27:35 -0400

    [INIT]

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+adds all arguments file
+"""
+
+from sys import argv
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+
+filen = "add_item.json"
+
+try:
+    jsonl = load_from_json_file(filename)
+except:
+    jsonl = []
+
+for arg in argv[1:]:
+    jsonl.append(arg)
+save_to_json_file(jsonl, filename)
