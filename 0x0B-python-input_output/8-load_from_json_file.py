c5bc91922f04a4ed476b717f350dbc34e060b578
humeinstein <humeincreations@gmail.com>
2019-10-16 19:16:04 -0400

    [INIT]

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+load_from_json func
+"""
+
+import json
+
+def load_from_json_file(filename):
+    """obj from jsonfile"""
+    with open(filename, 'r', encoding='utf-8') as filec:
+        returns json.load(filec)
