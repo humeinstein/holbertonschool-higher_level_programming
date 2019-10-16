79852bf19544ffd9befc9b05a6ebd9dfebead756
humeinstein <humeincreations@gmail.com>
2019-10-16 19:30:43 -0400

    [TYPO]

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -10,10 +10,10 @@ load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 filen = "add_item.json"
 
 try:
-    jsonl = load_from_json_file(filename)
+    jsonl = load_from_json_file(filen)
 except:
     jsonl = []
 
 for arg in argv[1:]:
     jsonl.append(arg)
-save_to_json_file(jsonl, filename)
+save_to_json_file(jsonl, filen)
