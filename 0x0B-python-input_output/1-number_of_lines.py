6408fdaadd5ed7d21194e47b8a05e65ad84547ce
humeinstein <humeincreations@gmail.com>
2019-10-16 18:53:33 -0400

    pep8

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -2,8 +2,12 @@
 """
 readfile&print
 """
+
+
 def number_of_lines(filename=""):
+
     with open(filename, encoding='utf-8') as filec:
         for i, l in enumerate(filec):
             pass
     return i + 1
+    
