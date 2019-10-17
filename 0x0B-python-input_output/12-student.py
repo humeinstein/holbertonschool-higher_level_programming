a901eb896942023949eb0c824b51090013424527
humeinstein <humeincreations@gmail.com>
2019-10-16 21:32:35 -0400

    init

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+class student
+"""
+
+class Student:
+    """dfine"""
+
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrive dict rep"""
+        if attrs is None or type(attrs) is not list:
+            return self.__dict__
+        for att in attrs:
+            if type(att) is not str:
+                return self.__dict__
+        return {x: self.__dict__[x] for x in self.__dict__ if x in attrs}
