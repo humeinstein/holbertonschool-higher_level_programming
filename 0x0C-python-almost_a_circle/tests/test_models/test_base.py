7d4b936f9a40deeb547f8ab4055fddeb390985dc
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2019-10-23 13:31:22 -0400

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-
-""" unitests """
-
-import pep8
+""" unit tests """
 import unittest
-import models.base
-import models.rectangle
-import models.square
-import json
+import pep8
+from models.rectangle import Rectangle
 
 
-
-class TestBase(unittest.TestCase):
-        """ testing """
-        def test_pep8_conformance(self):
-                """test that we conform to pep8"""
-
-                pep8style = pep8.StyleGuide(quiet=True)
-                result = pep8style.check_files(["models/base.py"])
-                self.ae(result.total_errors, 0,
-                        "Found code style errors (and warnings).")
+class TestRectangle(unittest.TestCase):
+    """ Class rectangle tests """
+    def test_pep8(self):
+        """ tests prp8 formating """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style erros (and warnings).")
