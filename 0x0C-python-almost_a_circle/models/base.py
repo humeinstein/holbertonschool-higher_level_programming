3be7ea3c399f3ad9eaa5c07d707549889d06ef9e
humeinstein <humeincreations@gmail.com>
2019-10-22 21:33:18 -0400

    udpate

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,13 +5,14 @@ base class
 import json
 import csv
 
+
 class Base:
     """base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
         """ class construc"""
-        if id is not None :
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
@@ -24,7 +25,7 @@ class Base:
             return []
         else:
             return json.dumps(list_dictionaries)
-    
+
     @classmethod
     def save_to_file(cls, list_objs):
         """ write json str rep to file """
@@ -64,9 +65,10 @@ class Base:
                 listo2 = cls.from_json_string(f.read())
             for i, x in enumerate(listo2):
                 listo2[i] = cls.create(**listo2[i])
-        except:
+        except BaseException:
             pass
         return listo2
+
     @classmethod
     def load_from_file_csv(cls):
         """ deserialize """
@@ -79,11 +81,12 @@ class Base:
                     for i, x in row.items():
                         row[i] = int(x)
                     newli = csv.DictReader(csf, fieldnames=fieldnames)
-                    newli = [dict([k, int(v)] for k, v in d.items())\
+                    newli = [dict([k, int(v)] for k, v in d.items())
                              for d in list_dictionaries]
                     return [cls.create(**d) for d in list_dictionaries]
-        except:
-                return []
+        except BaseException:
+            return []
+
     @classmethod
     def save_to_file_csv(cls, list_objs):
         """ serialize """
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,34 +5,37 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """ class """
+
     def __init__(self, size, x=0, y=0, id=None):
         """ square construct """
-        super().__init__(size, size, x, y , id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """ string rep """
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x, self.y, self.size)
-    
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x, self.y, self.size)
+
     @property
     def size(self):
         """ gets size """
         return self.width
+
     @size.setter
     def size(self, value):
         """ size setter """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value <= 0:
             raise ValueError("width must be > 0")
         else:
             self.width = value
-    
+
     def to_dictionary(self):
         squaredict = {
             'id': self.id,
             'x': self.x,
             'size': self.size,
-            'y': self.y         
+            'y': self.y
         }
         return squaredict
 
@@ -49,8 +52,8 @@ class Square(Rectangle):
                 elif key2 == 'y':
                     self.y = value
                 elif key2 == 'x':
-                    self.x = value     
-            return               
+                    self.x = value
+            return
         if precheck == 1:
             self.id = args[0]
             return
