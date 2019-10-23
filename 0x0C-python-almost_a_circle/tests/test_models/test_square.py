dcf896464e6aff3d25be297c94357330fcf48735
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2019-10-23 11:18:13 -0400

    Update test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -10,6 +10,6 @@ def test_pep8_conformance(self):
         """test that we conform to pep8"""
 
         pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(["models/square.py"])
+        result = pep8style.check_files(["square.py"])
         self.ae(result.total_errors, 0,
                 "Found code style errors (and warnings).")
