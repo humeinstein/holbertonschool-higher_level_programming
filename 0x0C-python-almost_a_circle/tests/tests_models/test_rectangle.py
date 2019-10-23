acec7d524182969c65952bc42794b5ac30a4faf2
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2019-10-22 21:40:53 -0400

    Update test_rectangle.py

--- 0x0C-python-almost_a_circle/tests/tests_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/tests_models/test_rectangle.py
@@ -12,24 +12,3 @@ Base = models.base.Base
 
 class RectangleTest(unittest.TestCase):
     """Test cases for rectangle"""
-
-    def setUp(self):
-        """set assertEquals to ae"""
-
-        self.ae = self.assertEqual
-
-    @classmethod
-    def tearDownClass(cls):
-        """tear down class"""
-
-        pass
-
-    def test_value_rect(self):
-        """test normal values"""
-
-        r1 = Rectangle(1, 2, 3, 4)
-
-        self.ae(r1.width, 1)
-        self.ae(r1.height, 2)
-        self.ae(r1.x, 3)
-        self.ae(r1.y, 4)
