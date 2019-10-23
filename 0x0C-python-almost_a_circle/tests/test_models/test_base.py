4df4fbd07c2654aa47ba2f10a8973277dac6d6f8
humeinstein <humeincreations@gmail.com>
2019-10-23 13:14:52 -0400

    pep8

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,11 +7,10 @@ from models.base import Base
 install pep8
 
 
-
 def test_pep8_conformance(self):
-        """test that we conform to pep8"""
+    """test that we conform to pep8"""
 
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(["models/base.py"])
-        self.ae(result.total_errors, 0,
-                "Found code style errors (and warnings).")
+    pep8style = pep8.StyleGuide(quiet=True)
+    result = pep8style.check_files(["models/base.py"])
+    self.ae(result.total_errors, 0,
+            "Found code style errors (and warnings).")
