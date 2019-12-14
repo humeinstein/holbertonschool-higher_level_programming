28c1e0e21a67a349644d56f7b92422e68240b04e
humeinste <humeincreations@gmail.com>
2019-12-14 20:09:11 +0000

    check

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python3
 """ list all states from database """
-if __name__ == "__main__":
-    
-    import MySQLdb
 
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    if len(sys.argv) == 4:
+        username = sys.argv[1]
+        upassword = sys.argv[2]
+        dbname = sys.argv[3]
+
+        db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=upassword,
+            db=dbname,
+            port=3306)
+
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        query_rows = cursor.fetchall()
+        for row in query_rows:
+            print("({}, '{}'".format(row[0], row[1]))
+
+        cursor.close()
+        db.close()
