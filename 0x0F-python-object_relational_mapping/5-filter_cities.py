e7c81558b5a6b64f6e240a855f5a5e0721405cf4
humeinste <humeincreations@gmail.com>
2019-12-15 16:32:40 +0000

    pep8

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -24,8 +24,7 @@ if __name__ == "__main__":
 
         cursor.execute(
             "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name=%s ORDER BY cities.id ASC;",
-            (stName,
-             ))
+            (stName,))
         query_rows = cursor.fetchall()
         rlen = len(query_rows)
         endo = 1
