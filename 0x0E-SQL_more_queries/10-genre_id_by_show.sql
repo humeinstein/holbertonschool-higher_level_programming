10b18eeed53f6b6ea5490d1dfeefbc09f4ee6061
humeinste <humeincreations@gmail.com>
2019-12-19 19:44:16 +0000

    bugfix

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
---List all shows that have one genre linked or more
+-- List all shows that have one genre linked or more
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_shows_genres.show_id
