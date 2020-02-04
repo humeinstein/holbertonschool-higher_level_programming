2ccb01d272e716874a02888281c6fedadbf5dcf8
humeinste <humeincreations@gmail.com>
2020-02-04 21:35:49 +0000

    edgecase?

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,6 +4,7 @@ function factorial (n) {
   const f = [];
   if (n === 0 || n === 1) { return 1; }
   if (f[n] > 0) { return f[n]; }
+  if (isNaN(n)) { return (1); }
   return (f[n] = factorial(n - 1) * n);
 }
 console.log(factorial(toCheck));
