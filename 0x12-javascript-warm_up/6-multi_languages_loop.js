a0892546b27c7b1e3cebd1aa34a771fcd502c58d
humeinste <humeincreations@gmail.com>
2020-02-04 19:27:25 +0000

    INIT: iter thru list and print each

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let iter = 0;
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+while (iter !== languages.length) {
+  console.log(languages[iter]);
+  iter++;
+}
