4f9119bc5c0aab7a2c58e36809de7bd2eec4a971
humeinste <humeincreations@gmail.com>
2020-02-04 21:32:29 +0000

    INIT: script computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const toCheck = process.argv[2];
+function factorial (n) {
+  const f = [];
+  if (n === 0 || n === 1) { return 1; }
+  if (f[n] > 0) { return f[n]; }
+  return (f[n] = factorial(n - 1) * n);
+}
+console.log(factorial(toCheck));
