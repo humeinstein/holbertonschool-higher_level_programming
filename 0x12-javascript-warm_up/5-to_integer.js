f39acd5750a798059a4afb0135232c72db937540
humeinste <humeincreations@gmail.com>
2020-02-04 19:22:21 +0000

    SEMISTYLE

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (isNaN(parseInt(process.argv[2])) === false) {
-    console.log('My number: '.concat(parseInt(process.argv[2])));
+  console.log('My number: '.concat(parseInt(process.argv[2])));
 } else {
-    console.log('Not a number')
+  console.log('Not a number');
 }
