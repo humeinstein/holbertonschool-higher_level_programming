edebeda4f175e4105d86c127664006636025f0cd
humeinste <humeincreations@gmail.com>
2020-02-10 19:51:49 +0000

    SEMISTANDARD

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,10 +3,10 @@
 const SquareBefore = require('./5-square.js');
 
 const Square = class Square extends SquareBefore {
-    charPrint (c) {
-	if (c === undefined) {this.print();} else {
-	    for (let iter = 0; iter < this.width; iter++) {console.log(c.repeat(this.height));}
-	}
+  charPrint (c) {
+    if (c === undefined) { this.print(); } else {
+      for (let iter = 0; iter < this.width; iter++) { console.log(c.repeat(this.height)); }
     }
+  }
 };
 module.exports = Square;
