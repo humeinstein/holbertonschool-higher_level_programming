498e6fc4b8c66298c8973d3777dfad47b28ab82a
humeinste <humeincreations@gmail.com>
2020-02-10 19:47:04 +0000

    INIT: i feel accomplished doing what I have done before 5 times in other languages..........

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// defines square inheirs from square from #5 char print
+const SquareBefore = require('./5-square.js');
+
+const Square = class Square extends SquareBefore {
+    charPrint (c) {
+	if (c === undefined) {this.print();} else {
+	    for (let iter = 0; iter < this.width; iter++) {console.log(c.repeat(this.height));}
+	}
+    }
+};
+module.exports = Square;
