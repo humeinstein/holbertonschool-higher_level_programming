2532411d73c4b6c64b5e351d6e120cc9ecebe050
humeinste <humeincreations@gmail.com>
2020-02-11 22:26:44 +0000

    should work

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,5 +6,5 @@ let url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, function (err, response, body) {
     if (err) { console.log(err); } else if (response.statusCode === 200) {
 	console.log(JSON.parse(body).title);
-    } else { console.log('Status code: ' + response.statusCode); }
+    } else { return; }
 });
