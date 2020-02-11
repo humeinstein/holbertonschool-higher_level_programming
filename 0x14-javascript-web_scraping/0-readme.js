1d99ad89c779c3ba129b67a1893141230c48a93a
humeinste <humeincreations@gmail.com>
2020-02-11 16:59:30 +0000

    INIT: file check print file or error

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script reads and prints contents of a file
+
+const fs = require('fs');
+const fileCheck = process.argv[2];
+fs.readFile(fileCheck, 'utf-8', function (err, data) {
+    if (err) { console.log(err); } else { console.log(data); }
+});
