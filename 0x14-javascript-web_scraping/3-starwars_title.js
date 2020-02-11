bf6dc2caab5e5f41edbf72523ce395580d6ac798
humeinste <humeincreations@gmail.com>
2020-02-11 22:28:15 +0000

    SEMITSTANDARD

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // script uses starwars api
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = 'http://swapi.co/api/films/' + process.argv[2];
 
 request(url, function (err, response, body) {
-    if (err) { console.log(err); } else if (response.statusCode === 200) {
-	console.log(JSON.parse(body).title);
-    } else { return; }
+  if (err) { console.log(err); } else if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  } else { }
 });
