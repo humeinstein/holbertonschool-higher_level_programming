520ff3d6bb7179976c416d705ca7d71c7e06cdc9
humeinste <humeincreations@gmail.com>
2020-02-11 17:28:49 +0000

    INIT: make get request and print error code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script display status code of a get request
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response) {
+    if (err) { console.log(err) } else { console.log('code: ' + response.statusCode); }
+});
