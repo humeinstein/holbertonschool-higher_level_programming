f00213099713d73363984a16eb18bb8f1e983813
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-02-13 18:02:10 -0500

    INIT: script gets all of the star wars movies and displays as list

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$(function () {
+    $.get('https://swapi.co/api/films/?format=json', function (data) {
+	for (const film of data.results) { $('#list_movies').append($('<li></li>').text(film.title)); }
+    });
+});
